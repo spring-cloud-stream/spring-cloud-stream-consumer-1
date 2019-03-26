64925ae568658b44f103c0b3cb32c4e0e18fe16c
506683059@qq.com <506683059@qq.com>
2019-03-26 15:13:27 +0800

    初始上传

---
+++ src/main/java/com/github/zk/springcloudstreamconsumer1/SpringCloudStreamConsumer1Application.java
@@ -0,0 +1,13 @@
+package com.github.zk.springcloudstreamconsumer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamConsumer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamConsumer1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/zk/springcloudstreamconsumer1/SpringCloudStreamConsumer1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.zk.springcloudstreamconsumer1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStreamConsumer1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
